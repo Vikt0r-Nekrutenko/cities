2b5c9fc0a9ec01fbe1bed9ee004f97af248fb680
BetterHire <hey@betterhire.me>
2019-09-18 23:30:28 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+vector<string> combine_cities(vector<string> available_cities);
+vector<string> read_available_cities();
+int write_to_file(vector<string> cities_list);
+
+int main() 
+{
+  vector<string> available_cities = read_available_cities();
+
+  vector<string> cities_list = combine_cities(available_cities); // TODO implement this function
+
+  return write_to_file(cities_list);
+}
+
+vector<string> combine_cities(vector<string> available_cities) {
+  // TODO replace with your solution!
+  return vector<string>();
+}
+
+vector<string> read_available_cities() {
+  string line;
+  ifstream input_file ("input.txt");
+  vector<string> available_cities;  
+  while ( getline (input_file,line) )
+  {
+    available_cities.push_back(line);
+  }
+  input_file.close();
+
+  return available_cities;
+}
+
+int write_to_file(vector<string> cities_list) {
+  ofstream output_file("./output.txt");
+  ostream_iterator<string> output_iterator(output_file, "\n");
+  copy(cities_list.begin(), cities_list.end(), output_iterator);
+
+  return 0;
+} 
