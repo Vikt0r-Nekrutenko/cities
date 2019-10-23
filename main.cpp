7cd8d0005ff8000fbb41f68bb7ca80ca24355673
Vikt0r <vikt0r.nekrut@outlook.com>
2019-10-23 20:21:18 +0300

    ants colony algorithm. too hard... :(

--- main.cpp
+++ main.cpp
@@ -4,39 +4,185 @@
 #include <fstream>
 #include <sstream>
 #include <iterator>
-#include <stack>
+#include <array>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 
-typedef struct _vertex_t{
-    uint16_t value;
-} vertex_t;
+const uint8_t MATRIX_SIZE = 26;
 
 typedef struct _edge_t{
-    uint16_t value;
-    uint16_t vrtx_to_end;
+    string value;
+    float  tao;
 } edge_t;
 
-typedef vector<vector<vertex_t>> adjacency_matrix_t;
-typedef vector<vector<edge_t>>   incidence_matrix_t;
+class vertex : public vector<edge_t> {
+public:
+    void print(){
+        for_each(begin(), end(), [](edge_t &edge){
+            cout << edge.value << " " << edge.value.size() << " " << edge.tao << endl;
+        });
+    }
 
-typedef struct _frame_t{
-    adjacency_matrix_t &matrix;
-    uint8_t target;
-} frame_t;
+    void sort() {
+        for(vertex::iterator it = vector<edge_t>::begin(); it != end() - 1; it++)
+            for(vertex::iterator sub_it = it + 1; sub_it != end(); sub_it++)
+                if(it->value.length() > sub_it->value.length())
+                    ::swap(*it, *sub_it);
+    }
+
+    void rm_passed_edge(){
+        pop_back();
+        mend = size() - 1;
+    }
+    void add_edge(edge_t edge){
+        push_back(edge);
+        mend = size() - 1;
+    }
+
+    float all_tao(){
+        float total_tao = 0;
+        for_each(begin(), end(), [&](edge_t &edge){
+            total_tao += edge.tao;
+        });
+        return total_tao;
+    }
+
+    size_t mend;
+};
+
+class row : private array<vertex, MATRIX_SIZE> {
+public:
+    using array<vertex, MATRIX_SIZE>::operator[];
+    void print(bool show_tao = false){
+        if(!show_tao)
+            for_each(begin(), end(), [](vertex &vrtx){
+                cout << vrtx.size() << (vrtx.size() > 9 ? " " : "  ");
+            });
+        else
+            for_each(begin(), end(), [](vertex &vrtx){
+                cout << vrtx.all_tao() << (vrtx.all_tao() > 9 ? " " : "  ");
+            });
+    }
+
+    void sort(){
+        for_each(begin(), end(), [](vertex &vrtx){
+            if(!vrtx.empty())
+                vrtx.sort();
+        });
+    }
+
+    vector<vertex*> row_except_empty_vertex(){
+        vector<vertex*> new_row;
+        for(row::iterator it = begin(); it != end(); it++)
+            if(!it->empty()/* && !it->front().is_passed*/)
+                new_row.push_back(it);
+        return new_row;
+    }
+
+    vertex *get_random_vertex(){
+        vector<vertex*> new_row = row_except_empty_vertex();
+        if(new_row.empty()) return nullptr;
+
+        edge_t *random_edge = nullptr;
+
+        for(size_t i = rand() % new_row.size(); i < new_row.size(); i++){
+            random_edge = &new_row[i]->back();
+            if(random_edge != nullptr)
+                return &at(random_edge->value.back() - 97);
+        }
+        return nullptr;
+    }
+
+    vertex *get_vertex_with_max_tao(){
+        vector<vertex*> new_row = row_except_empty_vertex();
+        if(new_row.empty()) return nullptr;
+
+        vertex *max_tao = new_row[0];
+
+        for(size_t i = 0; i < new_row.size(); i++){
+            if(max_tao->at(max_tao->mend).tao < new_row[i]->at(new_row[i]->mend).tao)
+                max_tao = new_row[i];
+        }
+        return max_tao;
+    }
+};
+
+class ant;
+
+class graph : private array<row, MATRIX_SIZE> {
+public:
+    using array<row, MATRIX_SIZE>::operator[];
+    graph(vector<string> source_list) : array<row, MATRIX_SIZE>() {
+        for_each(source_list.begin(), source_list.end(), [&](string word){
+            if(word.front() - 65 != word.back() - 97)
+                (*this)[word.front() - 65][word.back() - 97].add_edge({word, 0});
+        });
+    }
+    void print(){
+        for_each(begin(), end(), [](row &r){
+            r.print(true);
+            cout <<  endl;
+        });
+    }
+    void sort(){
+        for_each(begin(), end(), [](row &row_ref){
+            row_ref.sort();
+        });
+    }
+
+    void evaporation(ant &a);
+};
+
+class ant{
+public:
+    ant(graph &map) : personal_map(map), global_map(&map) {}
+    void move_to_next_vertex(uint8_t begin_vertex){
+        vertex *current_vrtx = personal_map[begin_vertex - 65].get_random_vertex();
+        do{
+            path.push_back(current_vrtx->back().value);
+            (*global_map)[current_vrtx->back().value.front() - 65][current_vrtx->back().value.back() - 97][current_vrtx->mend].tao+=3;
+            current_vrtx->rm_passed_edge();
+            current_vrtx = personal_map[path.back().back() - 97].get_random_vertex();
+        }while(current_vrtx != nullptr);
+    }
+    void move_to_next_vertex(){
+        vertex *current_vrtx = personal_map['E' - 65].get_vertex_with_max_tao();
+        do{
+            path.push_back(current_vrtx->back().value);
+            (*global_map)[current_vrtx->back().value.front() - 65][current_vrtx->back().value.back() - 97][current_vrtx->mend].tao++;
+            current_vrtx->rm_passed_edge();
+            if(rand() % 1)
+                current_vrtx = personal_map[path.back().back() - 97].get_vertex_with_max_tao();
+            else
+                current_vrtx = personal_map[path.back().back() - 97].get_random_vertex();
+        }while(current_vrtx != nullptr);
+    }
+
+    vector<string> path;
+    graph          personal_map;
+    graph          *global_map;
+};
+
+void graph::evaporation(ant &a){
+    size_t l = 0;
+    for(auto i : a.path)
+        l += i.length();
+
+    for(uint8_t i = 0; i < MATRIX_SIZE; i++)
+        for(uint8_t j = 0; j < MATRIX_SIZE; j++)
+            for(size_t x = 0; x < (*this)[j][i].size(); x++)
+                if((*this)[j][i][x].tao > 0)
+                    (*this)[j][i][x].tao = (1 - 0.2f)*(*this)[j][i][x].tao + 1/l;
+    for(auto i : a.path){
+        (*this)[i.front() - 65][i.back() - 97][]
+    }
+}
 
 vector<string> combine_cities(vector<string> available_cities);
 vector<string> read_available_cities();
 int write_to_file(vector<string> cities_list);
-adjacency_matrix_t build_adjacency_matrix(vector<string> &cities);
-incidence_matrix_t build_incidence_matrix();
-void print_matrix(adjacency_matrix_t &matrix);
-void print_matrix(incidence_matrix_t &matrix);
-string dfs(adjacency_matrix_t &mtrx, incidence_matrix_t &inc_mtrx, uint8_t target);
-string find_existing_edges(incidence_matrix_t &inc_mtrx, string &result);
-uint8_t find_max_edges_to_end(incidence_matrix_t &inc_mtrx, uint8_t row);
-uint8_t find_max_vertex(adjacency_matrix_t &mtrx, uint8_t target, uint8_t &next);
 
 int main() 
 {
@@ -47,86 +193,30 @@ int main()
   return write_to_file(cities_list);
 }
 
-void foo(vector<string> &available_cities, string &str){
-    uint32_t n = 0;
-    uint32_t l = 0;
-
-    while(n < str.size()){
-        for(vector<string>::iterator it = available_cities.begin(); it != available_cities.end(); it++){
-            if(it->front() == str[n] && it->back() == str[n+1]){
-//                strs.push_back(*it);
-                l += it->length();
-                it = available_cities.erase(it);
-            }
-        }
-        n += 2;
-    }
-}
-
-void sort_row(incidence_matrix_t &mtrx, uint8_t index){
-    for(uint8_t y = 0; y < 26; y++)
-        for(uint8_t x = 0; x < 26; x++)
-            if(mtrx[index][y].value > mtrx[index][x].value)
-                swap(mtrx[index][y], mtrx[index][x]);
-}
-
-void full_sort(incidence_matrix_t &mtrx){
-    for(uint8_t index = 0; index < 26; index++)
-        sort_row(mtrx, index);
-}
-
 vector<string> combine_cities(vector<string> available_cities) {
-  // TODO replace with your solution!
+    graph g1(available_cities);
+    g1.sort();
 
-    size_t size;
-    cin >> size;
-    string str;
-    vector<string> strs;
-    adjacency_matrix_t matrix;
-    incidence_matrix_t inc_mtrx = build_incidence_matrix();
+//    srand(time(0));
 
-    if(size == 2){
-        matrix = build_adjacency_matrix(available_cities);
-        str = dfs(matrix, inc_mtrx, 'e' - 97);
-        cout << str << " " << str.size() << endl;
-        foo(available_cities, str);
+    ant a1(g1);
+    a1.move_to_next_vertex('E');
+    g1.evaporation(a1);
 
-//        matrix = build_adjacency_matrix(available_cities);
-//        str += "...SPLEAT HERE!!!..." + dfs(matrix, inc_mtrx, 'g' - 97);
-////        cout << str << " " << str.size() << endl;
-//        foo(available_cities, str);
+//    int iter = 0;
+//    while(iter++ < 500){
+//        a1 = ant(g1);
+//        a1.move_to_next_vertex();
+//        g1.evaporation();
+////        cout << iter << " " << a1.path.size() << endl;
+//    }
 
-//        matrix = build_adjacency_matrix(available_cities);
-//        str += "...SPLEAT HERE!!!..." + dfs(matrix, inc_mtrx, 'b' - 97);
-////        cout << str << " " << str.size() << endl;
-//        foo(available_cities, str);
+//    a1 = ant(g1);
+//    a1.move_to_next_vertex();
+//    cout << a1.path.size() << endl;
 
-//        matrix = build_adjacency_matrix(available_cities);
-//        str += "...SPLEAT HERE!!!..." + dfs(matrix, inc_mtrx, 'p' - 97);
-//        cout << str << " " << str.size() << endl;
-//        foo(available_cities, str);
-
-        matrix = build_adjacency_matrix(available_cities);
-        print_matrix(matrix);
-
-//        cout << available_cities.size() << endl;
-    }
-    else{
-        vector<string> test_list(size == 1 ? 25 : size);
-        for(string &i : test_list) i.append({char('A'+rand()%('Z'-'A'+1)), char('a'+rand()%('z'-'a'+1))});
-        for(size_t i = test_list.size() - 1; i > 0; i--)
-            for(int j = i - 1; j >= 0; j--)
-                if(test_list[i].front() < test_list[j].front())
-                    swap(test_list[i], test_list[j]);
-//        for(string &i : test_list) cout << i << endl;
-        matrix = build_adjacency_matrix(test_list);
-        str = dfs(matrix, inc_mtrx, 'v' - 97);
-        cout << str << " " << str.length() << endl;
-    }
-
-//    cout << available_cities.size() << endl;
-
-    return strs;
+    g1.print();
+    return vector<string>();
 }
 
 vector<string> read_available_cities() {
@@ -142,83 +232,6 @@ vector<string> read_available_cities() {
   return available_cities;
 }
 
-string dfs(adjacency_matrix_t &mtrx, incidence_matrix_t &inc_mtrx, uint8_t target){
-    stack<frame_t> stk;
-    string result, comp;
-    stk.push({mtrx, target});
-
-    do{
-        for(uint8_t next = 0; next < 26; next++){
-            next = find_max_vertex(stk.top().matrix, stk.top().target, next);
-            if(stk.top().matrix[stk.top().target][next].value){
-                result.append({char(stk.top().target + 65), char(next + 97)});
-                stk.top().matrix[stk.top().target][next].value--;
-                inc_mtrx[stk.top().target][next].value++;
-                stk.push({stk.top().matrix, next});
-                next = 0;
-            }
-        }
-
-        if(result.size()){
-            string str = find_existing_edges(inc_mtrx, result);
-//            cout << result << "..." << str << " " << result.length() + str.length() << endl;
-            if(result.length() + str.length() > comp.length()) comp = result + str;
-            result.pop_back();
-            result.pop_back();
-        }
-        stk.pop();
-
-    }while(!stk.empty());
-
-    return comp;
-}
-
-uint8_t find_max_edges_to_end(incidence_matrix_t &inc_mtrx, uint8_t target){
-    uint8_t max = 0;
-    for(uint8_t i = 0; i < 26; i++){
-        if(inc_mtrx[target][i].vrtx_to_end >= inc_mtrx[target][max].vrtx_to_end && inc_mtrx[target][i].value){
-            max = i;
-        }
-    }
-    return max;
-}
-
-uint8_t find_max_vertex(adjacency_matrix_t &mtrx, uint8_t target, uint8_t &next){
-    for(uint8_t max = 0; max < 26; max++){
-        if(mtrx[target][max].value >= mtrx[target][next].value)
-            next = max;
-    }
-    return next;
-}
-
-string find_existing_edges(incidence_matrix_t &inc_mtrx, string &result){
-    incidence_matrix_t tmp_inc_mtrx = inc_mtrx;
-    string existing_edges;
-    uint8_t target = result.back() - 97;
-
-    for(size_t i = 0; i < result.size(); i += 2)
-        tmp_inc_mtrx[result[i] - 65][result[i + 1] - 97].value--;
-
-    uint8_t f = *(result.end() - 2) - 65;
-    uint8_t l = *(result.end() - 1) - 97;
-
-    for(uint8_t s = 0; s < 26; s++){
-        if(tmp_inc_mtrx[target][s].value){
-            uint8_t ss = find_max_edges_to_end(tmp_inc_mtrx, target);
-            if(tmp_inc_mtrx[target][ss].vrtx_to_end > tmp_inc_mtrx[target][s].vrtx_to_end && tmp_inc_mtrx[target][ss].value)
-                s = ss;
-            existing_edges.append({ char(target + 65), char(s + 97) });
-            tmp_inc_mtrx[target][s].value--;
-            inc_mtrx[f][l].vrtx_to_end++;
-//            cout << char(f+65) << char(l+97) << " ";
-            target = s;
-            s = 0;
-        }
-    }
-//    cout << endl;
-    return existing_edges;
-}
-
 int write_to_file(vector<string> cities_list) {
   ofstream output_file("./output.txt");
   ostream_iterator<string> output_iterator(output_file, "\n");
@@ -226,47 +239,4 @@ int write_to_file(vector<string> cities_list) {
 
   return 0;
 } 
-
-adjacency_matrix_t build_adjacency_matrix(vector<string> &source){
-    adjacency_matrix_t matrix(26);
-
-    uint16_t index = 0;
-    do{
-        matrix[index] = vector<vertex_t>(26);
-    }while(++index < 26);
-
-    for(vector<string>::iterator word = source.begin(); word != source.end(); word++){
-        matrix[word->front() - 65][word->back() - 97].value++;
-    }
-    return matrix;
-}
-
-incidence_matrix_t build_incidence_matrix(){
-    incidence_matrix_t matrix(26);
-
-    uint16_t index = 0;
-    do{
-        matrix[index] = vector<edge_t>(26);
-    }while(++index < 26);
-
-    return matrix;
-}
-
-void print_matrix(adjacency_matrix_t &mtrx){
-    for(uint8_t i = 'A'; i <= 'Z'; i++){
-        for(uint8_t j = 'a'; j <= 'z'; j++){
-            cout << i << j << " " << mtrx[i-65][j-97].value  << (mtrx[i-65][j-97].value > 9 ? " " : "  ");
-        }
-        cout << endl;
-    }
-}
-
-void print_matrix(incidence_matrix_t &mtrx){
-    for(uint8_t i = 'A'; i <= 'Z'; i++){
-        for(uint8_t j = 'a'; j <= 'z'; j++){
-            cout << i << j << " " << mtrx[i-65][j-97].value << ":" << mtrx[i-65][j-97].vrtx_to_end << " ";
-        }
-        cout << endl;
-    }
-}
 
