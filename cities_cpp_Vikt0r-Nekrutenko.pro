c5b533944bf70add5764a9a9cfd000a6d2ad5755
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-24 00:31:17 +0200

    000

--- cities_cpp_Vikt0r-Nekrutenko.pro
+++ cities_cpp_Vikt0r-Nekrutenko.pro
@@ -5,18 +5,3 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp \
-        sources/edge.cpp \
-        sources/matrix.cpp \
-        sources/row.cpp \
-        sources/vertex.cpp
-
-HEADERS += \
-    headers/colony.hpp \
-    headers/edge.hpp \
-    headers/matrix.hpp \
-    headers/vertex.hpp \
-    headers/row.hpp
-
-INCLUDEPATH += \
-    headers \
-    sources
--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <sstream>
 #include <string>
 #include <vector>
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <iterator>
-
-#include "colony.hpp"
 
 using namespace std;
 
@@ -13,38 +11,38 @@ vector<string> combine_cities(vector<string> available_cities);
 vector<string> read_available_cities();
 int write_to_file(vector<string> cities_list);
 
-int main() 
+int main()
 {
-  vector<string> available_cities = read_available_cities();
+    vector<string> available_cities = read_available_cities();
 
-  vector<string> cities_list = combine_cities(available_cities); // TODO implement this function
+    vector<string> cities_list = combine_cities(available_cities); // TODO implement this function
 
-  return write_to_file(cities_list);
+    return write_to_file(cities_list);
 }
 
-vector<string> combine_cities(vector<string> available_cities) {
-    colony ants_colony(available_cities);
-    return ants_colony.find_longest_path(1000).get_words();
+vector<string> combine_cities(vector<string> available_cities)
+{
+    return available_cities;
 }
 
-vector<string> read_available_cities() {
-  string line;
-  ifstream input_file ("input.txt");
-  vector<string> available_cities;  
-  while ( getline (input_file,line) )
-  {
-    available_cities.push_back(line);
-  }
-  input_file.close();
+vector<string> read_available_cities()
+{
+    string line;
+    ifstream input_file("input.txt");
+    vector<string> available_cities;
+    while (getline(input_file, line)) {
+        available_cities.push_back(line);
+    }
+    input_file.close();
 
-  return available_cities;
+    return available_cities;
 }
 
-int write_to_file(vector<string> cities_list) {
-  ofstream output_file("./output.txt");
-  ostream_iterator<string> output_iterator(output_file, "\n");
-  copy(cities_list.begin(), cities_list.end(), output_iterator);
+int write_to_file(vector<string> cities_list)
+{
+    ofstream output_file("./output.txt");
+    ostream_iterator<string> output_iterator(output_file, "\n");
+    copy(cities_list.begin(), cities_list.end(), output_iterator);
 
-  return 0;
-} 
-
+    return 0;
+}
