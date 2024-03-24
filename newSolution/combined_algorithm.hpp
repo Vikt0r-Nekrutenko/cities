2c7d6eff03af949dbee5a7e74f508f5eb9ed62a4
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-24 22:19:29 +0200

    039

--- newSolution/combined_algorithm.hpp
+++ newSolution/combined_algorithm.hpp
@@ -5,7 +5,60 @@
 
 Path combined_algorithm(Matrix &matrix)
 {
+    PathPairs antPathPairs;
+    Path bestPath;
+    size_t bestLength = 0;
+    int iterations = COLONY_ITERATIONS;
+    int ants = 1;//REGULAR_ANTS_COUNT + ELITE_ANTS_COUNT;
 
+    int vertexNumber = rand() % MATRIX_SIZE;
+    antPathPairs.push_back({{}, 0ull});
+
+    while(true) {
+        bool isEnd = true;
+        float totalProbability = 0.f;
+        for(auto &vertex : matrix.at(vertexNumber)) {
+            for(auto &edge : vertex) {
+                if(!edge.isPassed) {
+                    totalProbability += std::pow(edge.pheromone, ALPHA) * std::pow(edge.word->length() / 100.f, BETA);
+                    isEnd = false;
+                }}}
+
+        if(isEnd) { break; }
+
+        Edge *selectedEdge = nullptr;
+
+        float maxProbability = 0.f;
+        float currentProbability = 0.f;
+        float target = (float(rand()) / float(RAND_MAX)) * (0.99999f - 0.00001f) + 0.00001f;
+
+        for(auto &vertex : matrix.at(vertexNumber)) {
+            for(auto &edge : vertex) {
+                if(edge.isPassed)
+                    continue;
+                float probability = std::pow(edge.pheromone, ALPHA) * std::pow(float(edge.word->length()) / 100.f, BETA) / totalProbability;
+                currentProbability += probability;
+                if(ants > ELITE_ANTS_COUNT && target <= currentProbability){
+                    selectedEdge = &edge;
+                    goto endSelect;
+                } else if(ants <= ELITE_ANTS_COUNT && probability > maxProbability){
+                    maxProbability = probability;
+                    selectedEdge = &edge;
+                }
+            }
+        } endSelect:
+
+        vertexNumber = selectedEdge->word->back() - 'a';
+        selectedEdge->isPassed = true;
+        antPathPairs.back().first.push_back(selectedEdge);
+        antPathPairs.back().second += selectedEdge->word->length();
+    }
+
+    for(auto &edge : antPathPairs.back().first) {
+        edge->isPassed = false;
+    }
+
+    return bestPath;
 }
 
 #endif // COMBINED_ALGORITHM_HPP
