b6ce265c76eb22e8e68fd17b2bd0f8fa57160ba1
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-24 06:46:37 +0200

    035

--- newSolution/dfs_algorithm.hpp
+++ newSolution/dfs_algorithm.hpp
@@ -8,44 +8,48 @@ Path dfs_algorithm(Matrix &matrix)
     PathPairs pathPairs;
     Path bestPath;
     size_t bestLength = 0;
-    int vertexNumber = rand() % MATRIX_SIZE;
-    pathPairs.push_back({{}, 0ull});
+    int iterations = COLONY_ITERATIONS;
+    while(iterations--) {
+        int vertexNumber = rand() % MATRIX_SIZE;
+        pathPairs.push_back({{}, 0ull});
 
-    while(true) {
-        bool isEnd = true;
-        Edge *selectedEdge = nullptr;
-        for(auto &vertex : matrix.at(vertexNumber)) {
-            for(auto &edge : vertex) {
-                if(!edge.isPassed) {
-                    selectedEdge = &edge;
-                    selectedEdge->isPassed = true;
-                    isEnd = false;
-                    goto endSelect;
-                }}} endSelect:
+        while(true) {
+            bool isEnd = true;
+            Edge *selectedEdge = nullptr;
+            for(auto &vertex : matrix.at(vertexNumber)) {
+                for(auto &edge : vertex) {
+                    if(!edge.isPassed) {
+                        selectedEdge = &edge;
+                        isEnd = false;
+                        goto endSelect;
+                    }}} endSelect:
 
-        if(isEnd) {
-            if(pathPairs.back().first.size() == 1)
-                break;
-            if(bestLength < pathPairs.back().second) {
-                bestLength = pathPairs.back().second;
-                bestPath = pathPairs.back().first;
-                std::cout << pathPairs.size() << ":" << bestPath.size() << " [" << bestLength << "]" << std::endl;
+            if(isEnd) {
+                if(pathPairs.back().first.size() == 1)
+                    break;
+                if(bestLength < pathPairs.back().second) {
+                    bestLength = pathPairs.back().second;
+                    bestPath = pathPairs.back().first;
+                    std::cout << pathPairs.size() << ":" << bestPath.size() << " [" << bestLength << "]" << std::endl;
+                }
+                std::vector<Edge *> tmpPath {pathPairs.back().first.begin(), pathPairs.back().first.end() - 1};
+                size_t tmpLength = pathPairs.back().second - pathPairs.back().first.back()->word->length();
+
+                pathPairs.push_back({tmpPath, tmpLength});
+                vertexNumber = tmpPath.back()->word->back() - 'a';
+                continue;
             }
-            std::vector<Edge *> tmpPath {pathPairs.back().first.begin(), pathPairs.back().first.end() - 1};
-            size_t tmpLength = pathPairs.back().second - pathPairs.back().first.back()->word->length();
-            pathPairs.pop_back();
-            pathPairs.push_back({tmpPath, tmpLength});
-            vertexNumber = tmpPath.back()->word->back() - 'a';
-            continue;
-        }
 
-        vertexNumber = selectedEdge->word->back() - 'a';
-        selectedEdge->isPassed = true;
-        pathPairs.back().first.push_back(selectedEdge);
-        pathPairs.back().second += selectedEdge->word->length();
+            vertexNumber = selectedEdge->word->back() - 'a';
+            selectedEdge->isPassed = true;
+            pathPairs.back().first.push_back(selectedEdge);
+            pathPairs.back().second += selectedEdge->word->length();
+        }
+        for(auto &pathPair : pathPairs)
+            for(auto &edge : pathPair.first)
+                edge->isPassed = false;
+        pathPairs.clear();
     }
-    for(auto &edge : pathPairs.back().first)
-        edge->isPassed = false;
 
     return bestPath;
 }
