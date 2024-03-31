790c812411cb155c831adf5841d5a2b8b718e450
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-31 18:48:53 +0300

    075

--- newSolution/genetic_algorithm.hpp
+++ newSolution/genetic_algorithm.hpp
@@ -17,12 +17,12 @@ pair<Path, size_t> genetics_algorithm(Matrix &matrix, int generationsCount, int 
     vector<pair<Genome, size_t>> genomes(genomesPerGeneration);
 
     for(auto &genome : genomes) {
-        genome.first.regularAntCount    = randd(1, 26);
-        genome.first.eliteAntCount      = randd(1, 5);
-        genome.first.iterations         = randd(1, 30);
-        genome.first.evaporation        = randf(0.1f, 1.f);
-        genome.first.alpha              = randf(0.1f, 4.f);
-        genome.first.beta               = randf(0.1f, 4.f);
+        genome.first.regularAntCount    = randd(1, MAX_REGULAR_ANT_COUNT + 1);
+        genome.first.eliteAntCount      = randd(1, MAX_ELITE_ANT_COUNT + 1);
+        genome.first.iterations         = randd(1, MAX_ITERATIONS + 1);
+        genome.first.evaporation        = randf(0.1f, MAX_EVAPORATION);
+        genome.first.alpha              = randf(0.1f, MAX_ALPHA);
+        genome.first.beta               = randf(0.1f, MAX_BETA);
     }
     Genome bestGenome;
     int generationN = 0;
@@ -43,12 +43,12 @@ pair<Path, size_t> genetics_algorithm(Matrix &matrix, int generationsCount, int 
 
             if(i == mutableGenerationN) {
                 switch (mutableGenN) {
-                case 0: genomes[i].first.regularAntCount = randd(1, 27); break;
-                case 1: genomes[i].first.eliteAntCount   = randd(1, 6); break;
-                case 2: genomes[i].first.iterations      = randd(1, 31); break;
-                case 3: genomes[i].first.evaporation     = randf(0.1f, 1.f); break;
-                case 4: genomes[i].first.alpha           = randf(0.1f, 4.f); break;
-                case 5: genomes[i].first.beta            = randf(0.1f, 4.f); break;
+                case 0: genomes[i].first.regularAntCount = randd(1, MAX_REGULAR_ANT_COUNT + 1); break;
+                case 1: genomes[i].first.eliteAntCount   = randd(1, MAX_ELITE_ANT_COUNT + 1); break;
+                case 2: genomes[i].first.iterations      = randd(1, MAX_ITERATIONS + 1); break;
+                case 3: genomes[i].first.evaporation     = randf(0.1f, MAX_EVAPORATION); break;
+                case 4: genomes[i].first.alpha           = randf(0.1f, MAX_ALPHA); break;
+                case 5: genomes[i].first.beta            = randf(0.1f, MAX_BETA); break;
                 }
             }
         }
