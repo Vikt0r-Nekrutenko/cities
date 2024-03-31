0abd5a055a85b84bc1e2fd78b2e52d15d213bf5c
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-31 18:24:11 +0300

    070

--- newSolution/genetic_algorithm.hpp
+++ newSolution/genetic_algorithm.hpp
@@ -5,7 +5,22 @@
 
 pair<Path, size_t> genetics_algorithm(Matrix &matrix, int generationsCount, int genomesPerGeneration)
 {
+    vector<pair<Genome, size_t>> genomes(genomesPerGeneration);
 
+    for(auto &genome : genomes) {
+        genome.first.regularAntCount    = randd(1, 26);
+        genome.first.eliteAntCount      = randd(1, 5);
+        genome.first.iterations         = randd(1, 30);
+        genome.first.evaporation        = randf(0.1f, 1.f);
+        genome.first.alpha              = randf(0.1f, 4.f);
+        genome.first.beta               = randf(0.1f, 4.f);
+    }
+    Genome bestGenome;
+    int generationN = 0;
+
+    do {
+
+    } while(++generationN < generationsCount);
 }
 
 #endif // GENETIC_ALGORITHM_HPP
