023867e80a39ad385f3a8e9fe5952cfc7fcf8e95
Vikt0r-Nekrutenko <vikt0r.nekrut@outlook.com>
2024-03-31 18:33:40 +0300

    072

--- newSolution/genetic_algorithm.hpp
+++ newSolution/genetic_algorithm.hpp
@@ -24,6 +24,19 @@ pair<Path, size_t> genetics_algorithm(Matrix &matrix, int generationsCount, int 
         int mutableGenN = rand() % GEN_PER_GENOME;
         int mutableGenerationN = rand() % genomesPerGeneration;
 
+        for(int i = 0; i < int(genomes.size()); ++i) {
+            if(i == mutableGenerationN) {
+                switch (mutableGenN) {
+                case 0: genomes[i].first.regularAntCount = randd(1, 27); break;
+                case 1: genomes[i].first.eliteAntCount   = randd(1, 6); break;
+                case 2: genomes[i].first.iterations      = randd(1, 31); break;
+                case 3: genomes[i].first.evaporation     = randf(0.1f, 1.f); break;
+                case 4: genomes[i].first.alpha           = randf(0.1f, 4.f); break;
+                case 5: genomes[i].first.beta            = randf(0.1f, 4.f); break;
+                }
+            }
+        }
+
     } while(++generationN < generationsCount);
 }
 
